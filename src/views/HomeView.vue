db47ae946c75565ba083c12e04fa571d3c53b415
Aloisius Bagas <waloisiusbagas@gmail.com>
2024-09-22 22:46:56 +0700

    add hero

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,17 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
   <main>
-    <TheWelcome />
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+      <div class="hero-container">
+        <h1>Laura Thomson</h1>
+        <h2>I'm a Professional Photographer In New York City</h2>
+        <a href="#about" class="btn-scroll scrollto" title="Scroll Down"
+          ><i class="bx bx-chevrons-down"></i
+        ></a>
+      </div>
+    </section>
+    <!-- End Hero -->
   </main>
 </template>
